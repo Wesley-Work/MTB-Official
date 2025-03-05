5087bbe1d763337e842bac20d60e42d607cd4c16
Wesley <985189328@qq.com>
2025-03-05 19:36:22 +0800

    init

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,19 @@
 // src/router/index.ts
- 
+
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 import { RouterConfigMap } from '@config/routerMaps';
- 
+
 const routes: Array<RouteRecordRaw> = RouterConfigMap;
- 
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
- 
+
 // 在这里添加路由的导航守卫
 router.beforeEach((to, from, next) => {
-    console.log('Navigating to:', to.path);
-    next();
-  });
- 
+  console.log('Navigating to:', to.path);
+  next();
+});
+
 export default router;
