50e0f926d91cc370276271501a05f41bf892d1f3
Wesley <985189328@qq.com>
2025-03-18 14:09:09 +0800

    fix: lint

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,58 +2,58 @@ import { computed, defineComponent } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { version } from '@/package.json';
 import useFooter from '@hooks/useFooter';
-import { getDevice } from '@utils/device';
+// import { getDevice } from '@utils/device';
 import fhzsn_white from '@assets/fhzsn_white.png';
 import wesleyLogo from '@assets/source/wesley.svg';
 import '@style/footer.scss';
 // SVG 箭头图标组件
-const ArrowIcon = () => (
-  <svg viewBox="0 0 300 250" width="24" height="24" style={{ fill: '#ffffff', flexShrink: 0 }}>
-    <path d="M17,3h2c0.386,0 0.738,0.223 0.904,0.572c0.166,0.349 0.115,0.762 -0.13,1.062l-8.482,10.366l8.482,10.367c0.245,0.299 0.295,0.712 0.13,1.062c-0.165,0.35 -0.518,0.571 -0.904,0.571h-2c-0.3,0 -0.584,-0.135 -0.774,-0.367l-9,-11c-0.301,-0.369 -0.301,-0.898 0,-1.267l9,-11c0.19,-0.231 0.474,-0.366 0.774,-0.366z" />
-  </svg>
-);
+// const ArrowIcon = () => (
+//   <svg viewBox="0 0 300 250" width="24" height="24" style={{ fill: '#ffffff', flexShrink: 0 }}>
+//     <path d="M17,3h2c0.386,0 0.738,0.223 0.904,0.572c0.166,0.349 0.115,0.762 -0.13,1.062l-8.482,10.366l8.482,10.367c0.245,0.299 0.295,0.712 0.13,1.062c-0.165,0.35 -0.518,0.571 -0.904,0.571h-2c-0.3,0 -0.584,-0.135 -0.774,-0.367l-9,-11c-0.301,-0.369 -0.301,-0.898 0,-1.267l9,-11c0.19,-0.231 0.474,-0.366 0.774,-0.366z" />
+//   </svg>
+// );
 
 // 移动端固定菜单项配置
-const MOBILE_SECTIONS = [
-  {
-    title: '媒体部',
-    links: [
-      { label: '介绍', href: 'http://10.3.146.11/about-MTB' },
-      { label: '加入我们', href: 'http://10.3.146.11/join-us' },
-      { label: '<广告位招租>', href: 'javascript:void(0)' },
-    ],
-  },
-  {
-    title: '服务与支持',
-    links: [
-      { label: '共享网盘', href: 'http://10.3.146.11/share-network-disk' },
-      { label: '统一认证系统', href: 'http://10.3.146.13/' },
-      { label: '媒体部OA', href: 'http://10.3.146.12/' },
-    ],
-  },
-  {
-    title: '媒体账号',
-    links: [
-      {
-        label: '微信公众号',
-        href: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzg2MjYwMDE4MA==&scene=124#wechat_redirect',
-      },
-      { label: 'Bilibili', href: 'https://space.bilibili.com/257175059' },
-      {
-        label: '全媒体中心公众号',
-        href: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzkxNjI4MDk4MQ==&scene=124#wechat_redirect',
-      },
-    ],
-  },
-  {
-    title: '友情链接',
-    links: [
-      { label: '影视飓风', href: 'https://www.ysjf.com/' },
-      { label: '顺德中专门户网站', href: 'http://sdzz.net/' },
-      { label: '顺德区教育局', href: 'http://sdedu.net/' },
-    ],
-  },
-];
+// const MOBILE_SECTIONS = [
+//   {
+//     title: '媒体部',
+//     links: [
+//       { label: '介绍', href: 'http://10.3.146.11/about-MTB' },
+//       { label: '加入我们', href: 'http://10.3.146.11/join-us' },
+//       { label: '<广告位招租>', href: 'javascript:void(0)' },
+//     ],
+//   },
+//   {
+//     title: '服务与支持',
+//     links: [
+//       { label: '共享网盘', href: 'http://10.3.146.11/share-network-disk' },
+//       { label: '统一认证系统', href: 'http://10.3.146.13/' },
+//       { label: '媒体部OA', href: 'http://10.3.146.12/' },
+//     ],
+//   },
+//   {
+//     title: '媒体账号',
+//     links: [
+//       {
+//         label: '微信公众号',
+//         href: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzg2MjYwMDE4MA==&scene=124#wechat_redirect',
+//       },
+//       { label: 'Bilibili', href: 'https://space.bilibili.com/257175059' },
+//       {
+//         label: '全媒体中心公众号',
+//         href: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzkxNjI4MDk4MQ==&scene=124#wechat_redirect',
+//       },
+//     ],
+//   },
+//   {
+//     title: '友情链接',
+//     links: [
+//       { label: '影视飓风', href: 'https://www.ysjf.com/' },
+//       { label: '顺德中专门户网站', href: 'http://sdzz.net/' },
+//       { label: '顺德区教育局', href: 'http://sdedu.net/' },
+//     ],
+//   },
+// ];
 
 export default defineComponent({
   name: 'Footer',
